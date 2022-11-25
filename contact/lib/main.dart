60975e448aff38746aaa9ca3cfd462a9f365ee7b
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-11-25 19:30:35 +0900

    Contact_Recap-ListTile & StatefulWidget

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -12,50 +12,51 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var a = 1;
+  var userlist = ['김대연', '김소현', '김유민'];
+  var numlist = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              setState(() {
-                a++;
-              });
-            },
-            backgroundColor: Colors.black,
-            child: Text(
-              a.toString(),
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-            ),
-          ),
-          appBar: AppBar(
-            title: Text(
-              '연락처',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            toolbarHeight: 50,
-            elevation: 0,
-            backgroundColor: Colors.white,
-          ),
-          body: ListView.builder(
-            itemCount: 3,
-            itemBuilder: ((context, index) {
-              return contactStructure;
-            }),
-          ),
-          bottomNavigationBar: navBar),
-    );
+        home: Scaffold(
+      appBar: topBar,
+      bottomNavigationBar: navBar,
+      body: ListView.builder(
+        itemCount: userlist.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Text(numlist[index].toString()),
+            title: Text(userlist[index]),
+            trailing: TextButton(
+                onPressed: () {
+                  setState(() {
+                    numlist[index]++;
+                  });
+                },
+                child: Text('좋아요')),
+          );
+        },
+      ),
+    ));
   }
 }
 
 var contactStructure = ListTile(
   leading: Icon(Icons.account_circle),
   title: Text('홍길동'),
+);
+
+var topBar = AppBar(
+  title: Text(
+    '연락처',
+    style: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+  toolbarHeight: 50,
+  elevation: 0,
+  backgroundColor: Colors.white,
 );
 
 var navBar = BottomAppBar(
