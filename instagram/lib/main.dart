7e1ceda1545aa2cc077cd44fb0777f85dc0c8836
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-11-30 19:54:37 +0900

    Instagram_AppBar

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(MaterialApp(
+      theme: ThemeData(
+        iconTheme: IconThemeData(color: Colors.blue),
+        appBarTheme: AppBarTheme(color: Colors.grey),
+      ),
+      home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,8 +15,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Instagram')),
-      body: Text('Instagram'),
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.add_box_outlined,
+                color: Colors.black,
+              ))
+        ],
+        elevation: 0.3,
+        backgroundColor: Colors.white,
+      ),
+      body: null,
     );
   }
 }
