cd6bd59220d6671fb8620972e719f59f3898834d
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-12-03 20:44:35 +0900

    Insta_image picker initial settings

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,10 +1,10 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import './style.dart' as style;
 import './posting.dart' as posting;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import 'package:flutter/rendering.dart';
 
 void main() {
