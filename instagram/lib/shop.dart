789bd16035ad5402ffc99f48cfb9c713bf5a8bbd
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-12-12 15:13:25 +0900

    Insta_initial settings to bring data from firestore

--- instagram/lib/shop.dart
+++ instagram/lib/shop.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final firestore = FirebaseFirestore.instance;
+
 class Shop extends StatefulWidget {
   const Shop({super.key});
 
@@ -9,6 +11,18 @@ class Shop extends StatefulWidget {
 }
 
 class _ShopState extends State<Shop> {
+  getData() async {
+    var result =
+        await firestore.collection('product').doc('dyIp2wxEepvwMgvnDsFF').get();
+    print(result.get('brand'));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text('shopping tab');
