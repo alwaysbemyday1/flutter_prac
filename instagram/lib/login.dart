d5b83191090badc732fcbf3ae45ebefbda9094bb
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-12-12 22:29:29 +0900

    Insta_Created login&signIn functions

---
+++ instagram/lib/login.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final auth = FirebaseAuth.instance;
+final firestore = FirebaseFirestore.instance;
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginState();
+}
+
+class _LoginState extends State<LoginPage> {
+  createUser(email, password) async {
+    try {
+      var result = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      print(result.user);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  signIn(email, password) async {
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
