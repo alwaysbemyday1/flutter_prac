126cd07014e771e9583e99c193ca0c97fdd8817e
AlwaysBeMyDay1 <80505979+AlwaysBeMyDay1@users.noreply.github.com>
2022-12-13 11:53:12 +0900

    Ren:Insta_Connected pages to tab

--- instagram/lib/tabs/tab_page.dart
+++ instagram/lib/tabs/tab_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:instagram/tabs/account/account_page.dart';
+import 'package:instagram/tabs/feed/feed_page.dart';
+import 'package:instagram/tabs/searching/searching_page.dart';
+import 'package:instagram/tabs/shopping/shopping_page.dart';
 
 class TabPage extends StatefulWidget {
   const TabPage({super.key});
@@ -9,35 +13,34 @@ class TabPage extends StatefulWidget {
 
 class _TabPageState extends State<TabPage> {
   int _currentIndex = 0;
+  final _pages = const [
+    FeedPage(),
+    SearchingPage(),
+    ShoppingPage(),
+    AccountPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
         currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
         items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'search'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_bag),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-          ),
+              icon: Icon(Icons.shopping_bag), label: 'shopping_bag'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'person'),
         ],
       ),
     );
-    ;
   }
 }
